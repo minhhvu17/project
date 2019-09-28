44821121c53c377b8f88dad7445d245af061929b
minhhvu17 <ngxuanminhvu17@gmail.com>
2019-09-29 04:03:13 +0700

    k

---
+++ inside/inside.js
@@ -0,0 +1,319 @@
+const content = [
+    {
+        title: "Trạng Quỳnh",
+        picture: "./img/trangquynh.jpg",
+        status: "Hoàn tất",
+        director: "Đức Thịnh",
+        rate: "64%",
+        debut: "05/02/2019",
+        year: "2019",
+        time: "95 phút",
+        genre: "Hài hước, lãng mạn, cổ trang",
+        limitAge: "13+",
+        actor: [
+            {
+                realName: "Nhã Phương",
+                actName: "Điềm",
+            },
+            {
+                realName: "Quốc Anh",
+                actName: "Quỳnh",
+            },
+            {
+                realName: "Trấn Thành",
+                actName: "Xẩm",
+            },
+            {
+                realName: "Công Dương",
+                actName: "Trịnh Bá",
+            },
+            {
+                realName: "Khả Như",
+                actName: "ả Liễu",
+            },
+        ],
+        description: "Trạng Quỳnh sẽ xoay quanh hành trình giải cứu thầy Đoàn, đồng thời là cha Điềm đầy gian \n truân nhưng cũng nhiều tiếng cười của những con người nhỏ bé trong xã hội phong kiến dù luôn bị chèn ép bởi tầng lớp quan lại bất công.",
+        review: "",
+        trailer: "https://www.youtube.com/embed/",
+        ost: "https://www.youtube.com/embed/",
+    },
+
+    {//ve nha di con
+        title: "Về nhà đi con",
+        picture: "./img/venhadicon.jpg",
+        status: "Hoàn tất",
+
+        rate: "90%",
+        debut: "08/04/2019",
+        year: 2019,
+        director: "Nguyễn Danh Dũng",
+
+        time: "40 phút/tập",
+        genre: "Tâm lý, tình cảm gia đình",
+        limitAge: "Không",
+        actor: [
+            {
+                realName: "Bảo Hân",
+                actName: "Ánh Dương",
+            },
+            {
+                realName: "Trung Anh",
+                actName: "Ông Sơn",
+            },
+            {
+                realName: "Thư Quỳnh",
+                actName: "Thu Huệ",
+            },
+            {
+                realName: "Bảo Thanh",
+                actName: "Anh Thư",
+            },
+            {
+                realName: "Quốc Trường",
+                actName: "Vũ",
+            },
+        ],
+        description: "Câu chuyện phim Về nhà đi con xoay quanh 3 chị em gái Thu Huệ (Thu Quỳnh), Anh Thư (Bảo Thanh), Ánh Dương (Bảo Hân) mồ côi mẹ từ thuở nhỏ, mỗi người một tính cách, một lối sống. Chị cả Thu Huệ đảm đang, dịu dàng, điềm đạm; cô hai Anh Thư xinh đẹp, sắc sảo, thực dụng; cô út Ánh Dương bộc trực, hoang dã, như một cậu con trai. Cả ba chị em đều gặp phải những biến cố của riêng mình nhưng họ may mắn có chung một điểm tựa là ông Sơn (NSƯT Trung Anh) - một người bố tận tuỵ, hết mực yêu thương các con. Sau nhiều biến cố, cuối cùng, 3 cô con gái đều nhận ra hạnh phúc lớn nhất mà họ có được chính là một người bố luôn sẵn sàng bao bọc và nói về nhà đi con bất cứ khi nào họ cần một mái ấm.",
+        review: `Mặc dù đề tài phim là cuộc sống gia đình Việt Nam quen thuộc, <br/> Về nhà đi con đã tạo nên một cơn sốt trên truyền hình Việt Nam nói chung và VTV nói riêng trong năm 2019, 
+        trở thành món ăn tinh thần của một bộ phận phần đông khán giả phim truyền hình Việt Nam trong năm này. Dù chỉ vỏn vẹn với khoảng 30 phút mỗi tập, cùng với thời lượng dành cho quảng cáo không hề ít nhưng phim vẫn không hề giảm nhiệt. Sức hút của bộ phim được tạo ra và lan truyền trên mạng xã hội, một lý do quan trọng làm nên thành công này. Từng tập phim được khán giả trông mong, ngóng chờ mỗi thứ 2 đến thứ 6 hàng tuần; từng đoạn preview thu hút hàng triệu lượt xem trên Youtube hay Facebook; và cả những chi tiết phim cũng được quan tâm không kém. Cơn sốt Về nhà đi con trong năm 2019 còn làm bất ngờ cho ekip làm phim, trong đó có cả các diễn viên tham gia đóng phim; có diễn viên còn bị cộng đồng mạng tấn công và lăng mạ chỉ vì vai diễn phản diện trong phim. Đó còn là kịch bản độc đáo, đặc biệt còn được lấy cảm hứng từ Khi đàn ông góa vợ bật khóc từng ăn khách trên sóng VTV năm 2013, với nhiều éo le, ngang trái, đấy kịch tính và bất ngờ nhưng ẩn sau là những bài học đáng giá, những giây phút lắng đọng về ý nghĩa của gia đình trong đời sống mỗi con người. Nhiều khán giả bày tỏ nhìn thấy một phần hình ảnh người thân của mình trên phim, nhất là qua nhân vật người bố do NSƯT Trung Anh thể hiện. Cùng với đó là diễn xuất tự nhiên, chân thực, tính cách rõ rệt mang lại nhiều cảm xúc cho người xem của dàn diễn viên cũng đã góp phần vào thành công chung của tác phẩm.`,
+        trailer: "https://www.youtube.com/embed/vHdIXkg84QI",
+        ost: "https://www.youtube.com/embed/mRZxQCksyxo",
+    },
+
+    {
+        title: "",
+        picture: "./img/",
+        status: "Hoàn tất",
+        director: "",
+        rate: "",
+        debut: "",
+        year: "",
+        time: "",
+        genre: "",
+        limitAge: "",
+        actor: [
+            {
+                realName: "",
+                actName: "",
+            },
+            {
+                realName: "",
+                actName: "",
+            },
+            {
+                realName: "",
+                actName: "",
+            },
+            {
+                realName: "",
+                actName: "",
+            },
+            {
+                realName: "",
+                actName: "",
+            },
+        ],
+        description: "",
+        review: "",
+        trailer: "https://www.youtube.com/embed/",
+        ost: "https://www.youtube.com/embed/",
+    },
+
+    {
+        title: "",
+        picture: "./img/",
+        status: "Hoàn tất",
+        director: "",
+        rate: "",
+        debut: "",
+        year: "",
+        time: "",
+        genre: "",
+        limitAge: "",
+        actor: [
+            {
+                realName: "",
+                actName: "",
+            },
+            {
+                realName: "",
+                actName: "",
+            },
+            {
+                realName: "",
+                actName: "",
+            },
+            {
+                realName: "",
+                actName: "",
+            },
+            {
+                realName: "",
+                actName: "",
+            },
+        ],
+        description: "",
+        review: "",
+        trailer: "https://www.youtube.com/embed/",
+        ost: "https://www.youtube.com/embed/",
+    },
+
+    {
+        title: "",
+        picture: "./img/",
+        status: "Hoàn tất",
+        director: "",
+        rate: "",
+        debut: "",
+        year: "",
+        time: "",
+        genre: "",
+        limitAge: "",
+        actor: [
+            {
+                realName: "",
+                actName: "",
+            },
+            {
+                realName: "",
+                actName: "",
+            },
+            {
+                realName: "",
+                actName: "",
+            },
+            {
+                realName: "",
+                actName: "",
+            },
+            {
+                realName: "",
+                actName: "",
+            },
+        ],
+        description: "",
+        review: "",
+        trailer: "https://www.youtube.com/embed/",
+        ost: "https://www.youtube.com/embed/",
+    },
+
+    {
+        title: "",
+        picture: "./img/",
+        status: "Hoàn tất",
+        director: "",
+        rate: "",
+        debut: "",
+        year: "",
+        time: "",
+        genre: "",
+        limitAge: "",
+        actor: [
+            {
+                realName: "",
+                actName: "",
+            },
+            {
+                realName: "",
+                actName: "",
+            },
+            {
+                realName: "",
+                actName: "",
+            },
+            {
+                realName: "",
+                actName: "",
+            },
+            {
+                realName: "",
+                actName: "",
+            },
+        ],
+        description: "",
+        review: "",
+        trailer: "https://www.youtube.com/embed/",
+        ost: "https://www.youtube.com/embed/",
+    }
+
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+var realName, actName;
+
+const elementFilm = document.getElementById("data-film")
+
+var act1, act2, act3, act4, act5
+var indexAct = 0
+content.map(item => {
+
+    act1 = item.actor[0]
+    act2 = item.actor[1]
+    act3 = item.actor[2]
+    act4 = item.actor[3]
+    act5 = item.actor[4]
+
+
+    const html = `<div class="webback">
+    <div class="movie-title" id="data-film">${item.title}</div>
+    <div class="movie-info">
+        <div class="display-row">
+            <img class="film-poster" src=${item.picture} alt="poster">
+            <div class="display-column">
+                <div class="movie-status">
+                    Trạng thái: <span>${item.status}</span>
+                    <br>Yêu thích: <span>${item.rate}</span>
+                    <br>Đạo diễn: <span>${item.director}</span>
+                    <br>Năm: <span>${item.year}</span>
+                    <br>Ngày ra rạp: <span>${item.debut}</span>
+                    <br>Thời lượng: <span>${item.time}</span>
+                    <br>Thể loại: <span>${item.genre}</span>
+                    <br>Giới hạn tuổi: <span>${item.limitAge}</span>
+                    <br>Diễn viên chính: <span id="red">
+                        <a data-toggle="tooltip" title="${act1.actName}">${act1.realName}</a>,
+                        <a data-toggle="tooltip" title="${act2.actName}">${act2.realName}</a>,
+                        <a data-toggle="tooltip" title="${act3.actName}">${act3.realName}</a>,
+                        <a data-toggle="tooltip" title="${act4.actName}">${act4.realName}</a>,
+                        <a data-toggle="tooltip" title="${act5.actName}">${act5.realName}</a>
+                    </span>
+                    <br>Giới thiệu phim: <span id="yellow">${item.description}</span>
+                </div>
+
+            </div>
+
+        </div>
+        <div class="des-box">
+            <div class="description-title"><br>[GÓC REVIEW]</div>
+            <div class="description">
+                <div>
+                    ${item.review}
+                </div><br>
+
+            </div>
+        </div>
+        <div class="film-video">
+            <p>Official Trailer</p>
+            <iframe class="trailer" width="1080" height="604.7" src=${item.trailer}
+                frameborder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen></iframe>
+            <p>Official OST</p>
+            <iframe class="trailer" width="1080" height="604.7" src=${item.ost}
+                frameborder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen></iframe>
+        </div>
+    </div>`
+    elementFilm.insertAdjacentHTML("beforeend", html)
+})
